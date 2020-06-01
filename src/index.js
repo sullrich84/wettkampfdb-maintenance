e94c0643a827304dc57dc172fcd9d357bd83ef59
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2020-06-01 17:54:04 +0200

    Inital commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import logo from "./logo.svg";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className="card">
+      <img id="logo" src={logo} alt="Logo" />
+      <div class="row">
+        <h1>Wartung</h1>
+        <p>
+          Die WettkampfDB wird momentan aktualisiert. <br />
+          Bitte schau später nochmal vorbei&#8230;
+        </p>
+      </div>
+      <div class="row" style={{ color: "#666" }}>
+        <h1>Maintenance</h1>
+        <p>
+          WettkampfDB is currently being updated. <br />
+          Please check back later&#8230;
+        </p>
+      </div>
+    </div>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
