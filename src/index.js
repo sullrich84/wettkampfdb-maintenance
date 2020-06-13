2a6568dbff66d1b739508b89c697979e76d37934
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2020-06-13 17:57:26 +0200

    Minor style updates

--- src/index.js
+++ src/index.js
@@ -15,13 +15,20 @@ ReactDOM.render(
           Bitte schau später nochmal vorbei&#8230;
         </p>
       </div>
-      <div class="row" style={{ color: "#666" }}>
+      <div class="row font-gray">
         <h1>Maintenance</h1>
         <p>
           WettkampfDB is currently being updated. <br />
           Please check back later&#8230;
         </p>
       </div>
+      <footer>
+        © 2020 Sebastian Ullrich
+        <span role="img" aria-label="img">
+          ⚒️
+        </span>
+        Gemacht im Ruhrgebiet
+      </footer>
     </div>
   </React.StrictMode>,
   document.getElementById("root")
